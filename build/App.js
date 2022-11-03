d091b743913e77e8532af97cf6be5a10df73a859
Andriy Konoval <ceperiv@dmail.com>
2022-11-03 12:23:25 +0200

    After Effects

---
+++ build/App.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var react_1 = __importDefault(require("react"));
+require("./App.css");
+function App() {
+    return (<div className="App">
+     <h1>Hello:)</h1>
+    </div>);
+    alert('hello');
+}
+exports["default"] = App;
---
+++ build/index.js
@@ -0,0 +1,20 @@
+// "use strict";
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+
+// export const __esModule = true;
+// var react_1 = __importDefault(require("react"));
+// var client_1 = __importDefault(require("react-dom/client"));
+// // import "./index.css";
+// var App_1 = __importDefault(require("./App"));
+// var reportWebVitals_1 = __importDefault(require("./reportWebVitals"));
+// var root = client_1["default"].createRoot(document.getElementById('root'));
+// root.render(<react_1["default"].StrictMode>
+//     <App_1["default"] />
+//   </react_1["default"].StrictMode>);
+alert('hi');
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+(0, reportWebVitals_1["default"])();
